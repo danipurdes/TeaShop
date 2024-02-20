d585a101daa89c1646ad87b1e9092f48228f34d2
Danielle Purdes <daniellepurdes@gmail.com>
2024-02-19 19:03:57 -0800

    Added flavors and ingredients
    Tea bricks can now have five flavors: grassy, floral, fruity, earthy, smoky
    Can use ingredients on tea bricks to alter the flavor of the tea

--- scripts/TeaBrick.gd
+++ scripts/TeaBrick.gd
@@ -3,25 +3,41 @@ extends Area3D
 @export var item_type = "tea_brick"
 @export var mat_green_tea: Material
 @export var mat_black_tea: Material
+var flavor_profile = FlavorProfile.new()
+var tea: Constants.ingredients
+var ingredientList = []
 
-var tea: Constants.tea_type
 @onready var teaMatMap = {
-	Constants.tea_type.GREEN_TEA: mat_green_tea,
-	Constants.tea_type.BLACK_TEA: mat_black_tea
+	Constants.ingredients.GREEN_TEA: mat_green_tea,
+	Constants.ingredients.BLACK_TEA: mat_black_tea
 }
 
 func _ready():
-	$Label.text = getName()
+	updateLabel()
 	$Mesh.set_surface_override_material(0, teaMatMap[tea])
+
+func useItem(heldItem):
+	return heldItem.onUseItem(self)
 
 func onUseItem(pinger):
 	if "item_type" in pinger and pinger.item_type == "teapot":
 		get_node("/root/Node3D/Player").destroyHeldItem()
+		updateLabel()
 		return true
 	return false
 
 func setTea(newTeaType):
 	tea = newTeaType
+	flavor_profile.addIngredient(newTeaType)
+	updateLabel()
+
+func addIngredient(newIngredient):
+	flavor_profile.addIngredient(newIngredient)
+	ingredientList.append(newIngredient)
+	updateLabel()
+
+func updateLabel():
+	$Label.text = getName()
 
 func getName():
-	return item_type + "_" + str(tea)
+	return item_type + "\n" + Constants.ingredients.keys()[tea] + (" blend" if ingredientList.size() > 0 else "") + "\n" + flavor_profile._to_string()
--- scripts/TeaCup.gd
+++ scripts/TeaCup.gd
@@ -2,13 +2,13 @@ extends Area3D
 
 @export var item_type = "teacup"
 @export var state = "empty"
-@export var tea_type = Constants.tea_type.NONE
 @export var green_tea:Material
 @export var black_tea:Material
+var flavor_profile = FlavorProfile.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = getName()
+	updateLabel()
 
 func useItem(heldItem):
 	if heldItem.item_type == "teakettle":
@@ -18,18 +18,18 @@ func useItem(heldItem):
 			return true
 	if heldItem.item_type == "teapot":
 		if heldItem.state != "empty" and state == "empty":
-			if heldItem.tea_type != Constants.tea_type.NONE:
-				if heldItem.onUseItem(self):
-					updateState("hot_water")
-					updateTeaType(heldItem.tea_type)
-					return true
+			if heldItem.onUseItem(self):
+				updateState("hot_water")
+				updateLiquidMaterial()
+				updateFlavorProfile(heldItem.flavor_profile)
+				return true
 	return false
 
 func onUseItem(pinger):
 	if "machine_type" in pinger and pinger.machine_type == "sink":
 		if state != "empty" and state != "dirty":
+			flavor_profile.clearFlavorProfile()
 			updateState("dirty")
-			updateTeaType(Constants.tea_type.NONE)
 			return true
 		elif state == "dirty":
 			updateState("empty")
@@ -38,23 +38,25 @@ func onUseItem(pinger):
 
 func updateState(newState):
 	state = newState
-	$Label.text = getName()
+	updateLabel()
 
-func updateTeaType(newTea):
-	tea_type = newTea
-	updateLiquidMaterial(tea_type)
-	$Label.text = getName()
+func updateFlavorProfile(newFlavorProfile):
+	flavor_profile.copyFlavorProfile(newFlavorProfile)
+	updateLabel()
 
-func updateLiquidMaterial(newTea):
-	match newTea:
-		Constants.tea_type.GREEN_TEA:
-			$tea_cup/tea_cup_liquid.visible = true
+func updateLiquidMaterial():
+#	match newTea:
+#		Constants.tea_type.GREEN_TEA:
+#			$tea_cup/tea_cup_liquid.visible = true
 			$tea_cup/tea_cup_liquid.set_surface_override_material(0, green_tea)
-		Constants.tea_type.BLACK_TEA:
-			$tea_cup/tea_cup_liquid.visible = true
-			$tea_cup/tea_cup_liquid.set_surface_override_material(0, black_tea)
-		_:
-			$tea_cup/tea_cup_liquid.visible = false
+#		Constants.tea_type.BLACK_TEA:
+#			$tea_cup/tea_cup_liquid.visible = true
+#			$tea_cup/tea_cup_liquid.set_surface_override_material(0, black_tea)
+#		_:
+#			$tea_cup/tea_cup_liquid.visible = false
+
+func updateLabel():
+	$Label.text = getName()
 
 func getName():
-	return item_type + "_" + state + "_" + str(tea_type)
+	return item_type + "\n" + state + "\n" + flavor_profile._to_string()
