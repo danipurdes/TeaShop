17a3bcbffcf183aa1d28f0e03ccdc3dbbdf3397c
Danielle Purdes <daniellepurdes@gmail.com>
2024-02-27 18:43:56 -0800

    Added shelf with multiple counter spots
    Lowered player hand, shrunk ingredient jar, fixed flavor profile ui rotation

--- scripts/ui_flavor_profile.gd
+++ scripts/ui_flavor_profile.gd
@@ -8,7 +8,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var camera3d = get_node("/root/Node3D/Player")
-	print(camera3d.rotation_degrees)
-	var new_rotation = camera3d.rotation_degrees + Vector3(0, 0, 0)
-	print(new_rotation)
+	#var new_rotation = camera3d.rotation_degrees + Vector3(0, 0, 0)
+	#set_rotation_degrees(new_rotation)
+	look_at(camera3d.position, Vector3.UP, true)
+	var new_rotation = Vector3(0, rotation_degrees.y, 0)
 	set_rotation_degrees(new_rotation)
