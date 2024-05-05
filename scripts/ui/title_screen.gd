ec035462594c98fd85a44a5adf75b81410805113
Danielle Purdes <daniellepurdes@gmail.com>
2024-05-04 22:45:49 -0700

    Added title screen
    Title screen despawns when you press Space
    Added left click art icon
    Layout improvements
    Font improvements

---
+++ scripts/ui/title_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var state: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		queue_free()
