247b934c6bfecc6ea0458058ba02f6be0e7d2625
Danielle Purdes <daniellepurdes@gmail.com>
2024-05-05 20:16:58 -0700

    Added start & quit buttons
    Created title theme for button styling
    Game now loads title screen
    Pressing start button loads the teashop scene

--- scripts/ui/title_screen.gd
+++ scripts/ui/title_screen.gd
@@ -6,7 +6,9 @@ var state: String
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		queue_free()
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/teashop.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
