49564d526b6b968518799b790c07f5a4a7bbb2fc
Danielle Purdes <daniellepurdes@gmail.com>
2024-05-07 17:54:20 -0700

    Fixed pause screen not closing when ESC pressed

--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -7,8 +7,8 @@ func _ready():
 	is_paused = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("escape"):
 		if !is_paused:
 			add_child(load("res://scenes/ui/pause_screen.tscn").instantiate())
-		is_paused != is_paused
+		is_paused = !is_paused
