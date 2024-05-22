8124df55c0129b6b8854110eb5dbc10c6233e7e1
Danielle Purdes <daniellepurdes@gmail.com>
2024-05-21 21:39:44 -0700

    Can close pause menu using B button

--- scripts/ui/pause_screen.gd
+++ scripts/ui/pause_screen.gd
@@ -5,7 +5,7 @@ func _ready():
 	$CenterContainer/HBoxContainer/ResumeButton.grab_focus()
 
 func _process(_delta):
-	if Input.is_action_just_pressed("escape"):
+	if Input.is_action_just_pressed("escape") or Input.is_action_just_pressed("ui_cancel"):
 		queue_free()
 
 func _on_resume_button_pressed():
