00d6de1a85fa80b4d6040dea5b458fd726f3f682
Danielle Purdes <daniellepurdes@gmail.com>
2024-05-31 09:05:43 -0700

    Fix: Machines working when output hasn't collected
    Objects created by an oxidizer or leaf crusher must be collected before the machine is allowed to run again

--- scripts/LeafCrusher.gd
+++ scripts/LeafCrusher.gd
@@ -3,12 +3,14 @@ extends StaticBody3D
 @export var machine_type = "leaf_crusher"
 @export var obj_leaf_tray: PackedScene
 
+@onready var currentItem = null
+
 func _ready():
 	$StatusLight.light_color = Color.GREEN
 
 func useItem(heldItem):
 	if heldItem != null and heldItem.has_method("onUseItem") and heldItem.onUseItem(self):
-		if "item_type" in heldItem and heldItem.item_type == "basket":
+		if "item_type" in heldItem and heldItem.item_type == "basket" and currentItem == null:
 			startCrushLeaves()
 			return true
 	return false
@@ -24,4 +26,10 @@ func stopCrushLeaves():
 	var newLeafTray = obj_leaf_tray.instantiate()
 	newLeafTray.position = $LeafTraySpawn.global_position
 	newLeafTray.setTea(Constants.ingredients.GREEN_TEA)
+	currentItem = newLeafTray
+	newLeafTray.obj_attached_to = self
 	get_node("/root/Node3D").add_child(newLeafTray)
+
+func takeItem():
+	currentItem.obj_attached_to = null
+	currentItem = null
