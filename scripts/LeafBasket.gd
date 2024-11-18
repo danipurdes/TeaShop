3ba06c3878802af9e52e11cb9a0440f0587d3978
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-18 11:28:24 -0800

    Change to dynamic label, simplify onUseItem, add deposit and withdraw funcs

--- scripts/LeafBasket.gd
+++ scripts/LeafBasket.gd
@@ -3,31 +3,35 @@ extends Area3D
 signal state_changed(state_text)
 
 @export var item_type = "basket"
-@export var leaf_count = 0
-@export var leaf_count_max = 5
+@export_range(0,100) var leaf_count_max = 20
+
+var leaf_count_current = 0
 var obj_attached_to = null
 
 func _ready():
-	updateLabel(getName())
-	state_changed.connect(updateLabel)
+	state_changed.connect($Label.onLabelUpdate)
+	state_changed.emit(getName())
 	
-func onUseItem(itemToUseOn):
-	if "machine_type" in itemToUseOn and itemToUseOn.machine_type == "tea_tree":
-		return tryUpdateLeafCount(1)
-	elif "machine_type" in itemToUseOn and itemToUseOn.machine_type == "leaf_crusher":
-		return tryUpdateLeafCount(-1)
+func onUseItem(_itemToUseOn):
 	return false
 
-func tryUpdateLeafCount(deltaLeaf):
-	var newLeafCount = leaf_count + deltaLeaf
-	if newLeafCount <= leaf_count_max and newLeafCount >= 0:
-		leaf_count = newLeafCount
-		state_changed.emit(getName())
-		return true
-	return false
+func deposit_leaves(requested_amount):
+	if requested_amount <= 0:
+		return 0
+	
+	var leaves_deposited = min(leaf_count_max - leaf_count_current, requested_amount)
+	leaf_count_current += leaves_deposited
+	state_changed.emit(getName())
+	return leaves_deposited
+
+func withdraw_leaves(requested_amount):
+	if requested_amount <= 0:
+		return 0
+	
+	var leaves_withdrawn = min(requested_amount, leaf_count_current)
+	leaf_count_current -= leaves_withdrawn
+	state_changed.emit(getName())
+	return leaves_withdrawn
 
 func getName():
-	return str(leaf_count)
-
-func updateLabel(new_label_text):
-	$Label.text = new_label_text
+	return str(leaf_count_current) + "/" + str(leaf_count_max)
