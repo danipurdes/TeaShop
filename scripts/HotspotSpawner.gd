a9a974921af2842abf78d7196a37b147f66257bc
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-18 11:38:05 -0800

    Add allow_list param to SpawnHotspotObjects func, add null checks

--- scripts/HotspotSpawner.gd
+++ scripts/HotspotSpawner.gd
@@ -1,24 +1,27 @@
 class_name HotspotSpawner
 
-func _init():
-	pass
-
-func SpawnHotspotObjects(spawnObjs, hotspots, spawnTeas):
+func SpawnHotspotObjects(objs_to_spawn, hotspots, ingredients_to_spawn, allow_list):
 	# Validate args
-	if spawnObjs == null:
+	if objs_to_spawn == null:
 		printerr("HotspotSpawner: spawnObjs is null")
 		return
 	if hotspots == null:
 		printerr("HotspotSpawner: hotspots is null")
 		return
-	if spawnObjs.size() != hotspots.size():
-		printerr("HotspotSpawner: mismatch in size between spawnObjs and hotspots")
+	if objs_to_spawn.size() != hotspots.size():
+		printerr("HotspotSpawner: size mismatch between objs_to_spawn and hotspots")
+	
+	if ingredients_to_spawn != null and objs_to_spawn.size() != ingredients_to_spawn.size():
+		printerr("HotspotSpawner: mismatch in size between objs_to_spawn and ingredients_to_spawn")
 		return
 	
 	# Spawn objects on their hotspots
-	for index in spawnObjs.size():
-		if spawnObjs[index] != null and hotspots[index] != null:
-			hotspots[index].spawn_requested.emit(
-				spawnObjs[index].instantiate(),
-				spawnTeas[index] if spawnTeas != null else Constants.ingredients.NONE
-			)
+	for index in objs_to_spawn.size():
+		if objs_to_spawn[index] == null or hotspots[index] == null:
+			continue
+
+		hotspots[index].spawn_requested.emit(
+			objs_to_spawn[index].instantiate(),
+			ingredients_to_spawn[index] if ingredients_to_spawn != null else Constants.ingredients.NONE,
+			allow_list
+		)
