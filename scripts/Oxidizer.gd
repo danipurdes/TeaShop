0b71173abfc710714ebe0974bb9221d9172ec73c
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-18 12:42:57 -0800

    Remove ad hoc hotspot/ingredient/material logic

--- scripts/Oxidizer.gd
+++ scripts/Oxidizer.gd
@@ -1,111 +1,108 @@
 extends StaticBody3D
 
-signal on_oxidize_enter
-signal on_oxidize_exit
-
-@export var machine_type = "oxidizer"
-@export var idle_material: Material
-@export var started_material: Material
-@export var obj_ingredient: PackedScene
+@export var machine_type:String = "oxidizer"
+@export var idle_material:StandardMaterial3D
+@export var started_material:StandardMaterial3D
+@export var obj_ingredient:PackedScene
 @export var greenStyle:StyleBoxFlat
 @export var blackStyle:StyleBoxFlat
 
-@onready var indicator_mat = StandardMaterial3D.new()
+@onready var ingredients = Ingredients.new()
+@onready var indicator_mat:StandardMaterial3D = StandardMaterial3D.new()
+@onready var progress_bar = $SubViewport/CanvasLayer/ProgressBar
 
-var tea = Constants.ingredients.NONE
-var state = "idle"
-var currentItem = null
+var state:String = "idle"
 
 func _ready():
-	updateStatusLabel("Inactive")
+	ingredients.ingredients_changed.connect(on_ingredients_changed)
 	$IndicatorMesh.set_surface_override_material(0, idle_material)
 
 func _process(_delta):
 	if !$GreenTeaTimer.is_stopped():
-		$SubViewport/CanvasLayer/ProgressBar.value = $SubViewport/CanvasLayer/ProgressBar.max_value * (1 - ($GreenTeaTimer.time_left / $GreenTeaTimer.wait_time))
+		progress_bar.value = progress_bar.max_value * (1 - ($GreenTeaTimer.time_left / $GreenTeaTimer.wait_time))
 	elif !$BlackTeaTimer.is_stopped():
-		$SubViewport/CanvasLayer/ProgressBar.value = $SubViewport/CanvasLayer/ProgressBar.max_value * (1 - ($BlackTeaTimer.time_left / $BlackTeaTimer.wait_time))
+		progress_bar.value = progress_bar.max_value * (1 - ($BlackTeaTimer.time_left / $BlackTeaTimer.wait_time))
 
-func useItem(item):
-	if item == null:
-		match state:
-			"started":
-				match tea:
-					Constants.ingredients.GREEN_TEA:
-						stopOxidizeLeaves()
-						return true
-					Constants.ingredients.BLACK_TEA:
-						stopOxidizeLeaves()
-						return true
-		return false
-	elif currentItem == null and item.has_method("onUseItem") and item.onUseItem(self):
-		if "item_type" in item and item.item_type == "leaf_tray":
-			startOxidizeLeaves()
-			return true
-	return false
+func useItem(held_item):
+	match state:
+		"idle":
+			if $CounterHotspot.currentItem != null:
+				return false
+			if !held_item.has_method("onUseItem") or !held_item.onUseItem(self):
+				return false
+			if "item_type" not in held_item:
+				return false
+			match held_item.item_type:
+				"leaf_tray":
+					startOxidizeLeaves()
+					return true
+				_:
+					return false
+		"started":
+			if held_item != null or ingredients.ingredients.size() == 0:
+				return false
+			match ingredients.ingredients[0]:
+				Constants.ingredients.GREEN_TEA:
+					stopOxidizeLeaves()
+					return true
+				Constants.ingredients.BLACK_TEA:
+					stopOxidizeLeaves()
+					return true
+				_:
+					return false
+		_:
+			return false
 
 func startOxidizeLeaves():
 	$GreenTeaTimer.start()
 	state = "started"
-	updateTeaType(Constants.ingredients.NONE)
-	$SubViewport/CanvasLayer/ProgressBar.add_theme_stylebox_override("fill", greenStyle)
+	ingredients.clearIngredients()
+	progress_bar.add_theme_stylebox_override("fill", greenStyle)
 
 func stopOxidizeLeaves():
 	spawnTeaBrick()
 	state = "idle"
-	updateTeaType(Constants.ingredients.NONE)
+	ingredients.clearIngredients()
 	$ParticleEmitter.emitting = true
 	$GreenTeaTimer.stop()
 	$BlackTeaTimer.stop()
-	$SubViewport/CanvasLayer/ProgressBar.value = $SubViewport/CanvasLayer/ProgressBar.min_value
+	progress_bar.value = progress_bar.min_value
 
 func _on_green_tea_timer_timeout():
-	updateTeaType(Constants.ingredients.GREEN_TEA)
+	ingredients.clearIngredients()
+	ingredients.addIngredient(Constants.ingredients.GREEN_TEA)
 	$BlackTeaTimer.start()
-	$SubViewport/CanvasLayer/ProgressBar.add_theme_stylebox_override("fill", blackStyle)
+	progress_bar.add_theme_stylebox_override("fill", blackStyle)
 
 func _on_black_tea_timer_timeout():
-	updateTeaType(Constants.ingredients.BLACK_TEA)
-	$SubViewport/CanvasLayer/ProgressBar.value = $SubViewport/CanvasLayer/ProgressBar.max_value
+	ingredients.clearIngredients()
+	ingredients.addIngredient(Constants.ingredients.BLACK_TEA)
+	progress_bar.value = progress_bar.max_value
 
-func updateTeaType(new_type):
-	tea = new_type
+func spawnTeaBrick():
+	if ingredients.ingredients.size() > 0:
+		$CounterHotspot.spawnObject(obj_ingredient.instantiate(), ingredients.ingredients[0], [])
+
+func update_ingredients_label(new_ingredients):
+	var output = ""
+	var nameKeys = Constants.ingredients.keys()
+	for ingredient in new_ingredients:
+		output += nameKeys[ingredient]
+	$IngredientLabel.onLabelUpdate(output)
+
+func on_ingredients_changed(_new_ingredients):
 	match state:
 		"idle":
 			$IndicatorMesh.set_surface_override_material(0, idle_material)
-			updateStatusLabel("Inactive")
+			$IngredientLabel.onLabelUpdate("Inactive")
+			return
 		"started":
-			match tea:
-				Constants.ingredients.GREEN_TEA:
-					updateMaterial(Constants.ingredients.GREEN_TEA)
-					updateStatusLabel("Green Tea")
-				Constants.ingredients.BLACK_TEA:
-					updateMaterial(Constants.ingredients.BLACK_TEA)
-					updateStatusLabel("Black Tea")
-				_:
-					$IndicatorMesh.set_surface_override_material(0, started_material)
-					updateStatusLabel("Working")
-
-func updateMaterial(e_ingredient):
-	indicator_mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_SCISSOR
-	var color_ingredient = Constants.ingredientColorMap[e_ingredient]
-	indicator_mat.albedo_color = color_ingredient
-	$IndicatorMesh.set_surface_override_material(0, indicator_mat)
-
-func spawnTeaBrick():
-	if tea != Constants.ingredients.NONE:
-		var newTeaBrick = obj_ingredient.instantiate()
-		if "tea" in newTeaBrick:
-			newTeaBrick.tea = tea
-		newTeaBrick.position = $TeaBrickSpawn.global_position
-		currentItem = newTeaBrick
-		newTeaBrick.obj_attached_to = self
-		get_node("/root/Node3D").add_child(newTeaBrick)
-		
-
-func takeItem():
-	currentItem.obj_attached_to = null
-	currentItem = null
-
-func updateStatusLabel(new_status):
-	$TeaLabel.text = new_status
+			if _new_ingredients.size() == 0:
+				$IndicatorMesh.set_surface_override_material(0, started_material)
+				$IngredientLabel.onLabelUpdate("Working")
+				return
+			$IndicatorMesh.set_surface_override_material(0, ingredients.ingredientsMat)
+			$IngredientLabel.onLabelUpdate(ingredients.ingredientsToString())
+			return
+		_:
+			return
