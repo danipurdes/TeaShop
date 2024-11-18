0814cddd85224e1699baf824dc806dd7a0ac3f6b
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-18 11:49:59 -0800

    Use basket's deposit_leaves func, change timeout signal to be through script, style refactor

--- scripts/TeaTree.gd
+++ scripts/TeaTree.gd
@@ -1,18 +1,33 @@
 extends Node3D
 
 @export var machine_type = "tea_tree"
+@onready var leaf_refresh_timer = $LeafRefreshTimer
+
+func _ready():
+	leaf_refresh_timer.timeout.connect(refresh_leaves)
 
 func useItem(heldItem):
-	if heldItem != null and heldItem.has_method("onUseItem") and heldItem.onUseItem(self):
-		trimLeaves()
-		return true
-	return false
+	if heldItem == null:
+		return false
+	if !heldItem.has_method("onUseItem"):
+		return false
+	if "item_type" not in heldItem:
+		return heldItem.onUseItem(self)
 
-func trimLeaves():
+	match heldItem.item_type:
+		"basket":
+			var leaves_deposited = heldItem.deposit_leaves(1)
+			if leaves_deposited > 0:
+				trim_leaves()
+			return leaves_deposited > 0
+		_:
+			return false
+
+func trim_leaves():
 	$LeavesMesh.visible = false
 	$Hitbox.set_disabled(true)
 	$LeafRefreshTimer.start()
 
-func _on_leaf_refresh_timer_timeout():
+func refresh_leaves():
 	$LeavesMesh.visible = true
 	$Hitbox.set_disabled(false)
