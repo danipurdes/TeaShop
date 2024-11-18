b7ed6ae191fe86c2976a890ef8a9b1c37ede459a
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-18 11:58:02 -0800

    Add signal object_taken, style refactor

--- scripts/Hotspot.gd
+++ scripts/Hotspot.gd
@@ -2,35 +2,44 @@ extends Area3D
 
 class_name Hotspot
 
-@export var SpawnObj: PackedScene
-@export var machine_type = "hotspot"
-@export var allowlist = []
+@export var SpawnObj:PackedScene
+@export var machine_type:String = "hotspot"
+@export var allow_list:Array[String] = []
 
 var currentItem = null
 
-signal spawn_requested(objToSpawn, spawnData)
+signal spawn_requested(obj_to_spawn, spawn_data, allow_list)
+signal object_taken()
 
 func _ready():
 	spawn_requested.connect(spawnObject)
 	if SpawnObj != null:
 		var spawn_obj = SpawnObj.instantiate()
 		spawn_obj.position = self.global_position
-		get_node("/root/Node3D").add_child(spawn_obj)
+		get_node("/root/Node3D").add_child.call_deferred(spawn_obj)
 
-func spawnObject(spawnObj, spawnData):
-	spawnObj.position = global_position
-	spawnObj.rotation = global_rotation
-	spawnObj.obj_attached_to = self
-	if spawnData != null and "tea" in spawnObj:
-		spawnObj.tea = spawnData
-	currentItem = spawnObj
-	get_node("/root/Node3D").add_child.call_deferred(spawnObj)
+func spawnObject(obj_to_spawn, spawn_data, new_allow_list):
+	obj_to_spawn.position = global_position
+	obj_to_spawn.rotation = global_rotation
+	obj_to_spawn.obj_attached_to = self
+	currentItem = obj_to_spawn
+
+	if spawn_data != null and "ingredient_on_spawn" in obj_to_spawn:
+		obj_to_spawn.ingredient_on_spawn = spawn_data
+
+	if new_allow_list.size() > 0:
+		allow_list = new_allow_list.duplicate
+
+	get_node("/root/Node3D").add_child.call_deferred(obj_to_spawn)
 
 func useItem(item):
 	if currentItem != null:
 		return false
-		
-	if item == null or !isItemAllowed(item.item_type):
+	if item == null:
+		return false
+	if "item_type" not in item:
+		return false
+	if !is_item_allowed(item.item_type):
 		return false
 	
 	currentItem = get_node("/root/Node3D/Player").requestDropHeldItem(self)
@@ -40,8 +49,7 @@ func useItem(item):
 func takeItem():
 	currentItem.obj_attached_to = null
 	currentItem = null
+	object_taken.emit()
 	
-func isItemAllowed(itemType):
-	if allowlist.size() > 0:
-		return itemType in allowlist
-	return true
+func is_item_allowed(itemType):
+	return allow_list.size() == 0 or itemType in allow_list
