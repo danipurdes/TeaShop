76215b4519b1fc08883c82a8667c373e54d6b7cb
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-18 11:59:01 -0800

    Simplify logic

--- scripts/counter.gd
+++ scripts/counter.gd
@@ -1,18 +1,13 @@
 extends StaticBody3D
 
+@export var allow_list:Array[String] = []
 @export var spawnObj:PackedScene
-@export var spawnTea:Constants.ingredients
+@export var spawnTea:Constants.ingredients = Constants.ingredients.NONE
 
-@onready var spawnObjs = [spawnObj]
-@onready var spawnTeas = [spawnTea]
-@onready var hotspots = [$CounterHotspot]
-var spawnedObj
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var spawner = HotspotSpawner.new()
-	spawner.SpawnHotspotObjects(spawnObjs, hotspots, spawnTeas)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+	if spawnObj != null:
+		$CounterHotspot.spawnObject(
+			spawnObj.instantiate(),
+			spawnTea,
+			allow_list
+		)
