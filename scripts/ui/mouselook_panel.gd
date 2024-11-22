8caeb38133184e3197e9497523891eb81a0b2a75
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-22 10:25:55 -0800

    Add action_changed signal

--- scripts/ui/mouselook_panel.gd
+++ scripts/ui/mouselook_panel.gd
@@ -5,47 +5,51 @@ extends PanelContainer
 var held_item = null
 var target_item = null
 
-signal use_label_changed(verb)
+signal use_label_changed(new_label)
+signal action_changed(verb)
+
+func _ready():
+	use_label_changed.connect(use_item_label.on_label_update)
 
 func on_held_item_changed(new_held_item):
 	visible = new_held_item != null
 	held_item = new_held_item
-	use_item_label.onLabelUpdate(fetchUseLabel())
+	use_label_changed.emit(fetch_use_label())
 
 func on_target_item_changed(new_target_item):
 	target_item = new_target_item
-	use_item_label.onLabelUpdate(fetchUseLabel())
+	use_label_changed.emit(fetch_use_label())
 
-func fetchUseLabel():
+func fetch_use_label():
 	if target_item == null:
-		use_label_changed.emit("normal")
+		action_changed.emit("normal")
 		return ""
 
 	if held_item == null:
 		if "item_type" in target_item:
-			use_label_changed.emit("up_arrow")
+			action_changed.emit("up_arrow")
 			return "pick up " + target_item.item_type
-		use_label_changed.emit("normal")
+		action_changed.emit("normal")
 		return ""
 	
 	if "machine_type" in target_item and "item_type" in held_item:
 		match target_item.machine_type:
 			"hotspot":
-				use_label_changed.emit("down_arrow")
+				action_changed.emit("down_arrow")
 				return "set down " + held_item.item_type
 			"hotplate":
-				use_label_changed.emit("down_arrow")
+				action_changed.emit("down_arrow")
 				return "set down " + held_item.item_type
 			_:
-				use_label_changed.emit("normal")
+				action_changed.emit("normal")
 				return ""
 
 	if held_item.has_method("onUseItem") and target_item.has_method("useItem"):
-		var held_itemKey = held_item.getName() if held_item.has_method("getName") else ""
-		var colliderKey = target_item.getName() if target_item.has_method("getName") else ""
-		var useKey = held_itemKey + "|" + colliderKey
-		use_label_changed.emit("normal")
-		return UseLabelUtility.itemUseLabelMap.get(useKey)
+		var held_item_key = held_item.getName() if held_item.has_method("getName") else ""
+		var target_key = target_item.getName() if target_item.has_method("getName") else ""
+		var use_key = held_item_key + "|" + target_key
+		action_changed.emit("normal")
+		return UseLabelUtility.itemUseLabelMap.get(use_key)
 	
-	use_label_changed.emit("normal")
+	action_changed.emit("normal")
 	return ""
