5360a98b93c4d401d3c8862b31e2e2e3f9f0f14b
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-22 10:34:38 -0800

    Refactor

--- scripts/crush_leaf_tray.gd
+++ scripts/crush_leaf_tray.gd
@@ -3,34 +3,31 @@ extends Area3D
 @export var item_type = "leaf_tray"
 @export var ingredient_on_spawn:Constants.ingredients = Constants.ingredients.GREEN_TEA
 
-@onready var ingredients = Ingredients.new()
+@onready var ingredients:Ingredients = $Blend.ingredients
 
 var obj_attached_to = null
 
 signal state_changed(state_text)
 
 func _ready():
-	ingredients.ingredients_changed.connect(onIngredientsChanged)
+	ingredients.ingredients_changed.connect(on_ingredients_changed)
 
-	match ingredient_on_spawn:
-		Constants.ingredients.NONE:
-			return
-		_:
-			ingredients.addIngredient(ingredient_on_spawn)
+	if ingredient_on_spawn != null and !ingredients.add_ingredient(ingredient_on_spawn):
+		print_debug("Failed to add spawn ingredient to " + item_type)
 
-func onUseItem(item_to_use_on):
-	if "machine_type" not in item_to_use_on:
+func onUseItem(target_item):
+	if "machine_type" not in target_item:
 		return false
 	
-	match item_to_use_on.machine_type:
+	match target_item.machine_type:
 		"oxidizer":
-			get_node("/root/Node3D/Player").destroyHeldItem()
+			# Replace with dequeue, have the player listen for the node being freed
+			get_node("/root/Node3D/Player").destroy_held_item()
 			return true
 		_:
 			return false
 
-func onIngredientsChanged(_new_ingredients):
-	$IngredientMesh.set_surface_override_material(0, ingredients.ingredientsMat)
+func on_ingredients_changed(_new_ingredient_list):
 	state_changed.emit(getName())
 
 func getName():
