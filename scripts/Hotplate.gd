d56cba8e83244fc1d8c3d7e275dc8b59397205ba
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-24 18:30:15 -0800

    Beginning to deprecate hotplate

--- scripts/Hotplate.gd
+++ scripts/Hotplate.gd
@@ -6,32 +6,40 @@ class_name Hotplate
 @export var machine_type:String = "hotplate"
 @export var allow_list:Array[String] = ["kettle"]
 
-var currentItem = null
-var obj_attached_to = null
+var state:String = "empty"
 
-signal state_changed(state_text)
+func _ready():
+	$CounterHotspot.allow_list = allow_list
+	$CounterHotspot.object_placed.connect(attach_item)
+	$CounterHotspot.object_taken.connect(take_item)
 
-func useItem(item):
-	if currentItem != null:
-		return false
-	if item == null:
-		return false
-	if "item_type" not in item:
-		return false
-	if !is_item_allowed(item.item_type):
-		return false
+func useItem(_item):
+	# if currentItem != null:
+	# 	return false
+	# if item == null:
+	# 	return false
+	# if "item_type" not in item:
+	# 	return false
+	# if !is_item_allowed(item.item_type):
+	# 	return false
+	# if !attach_item(get_node("/root/Node3D/Player").request_drop_held_item(self)):
+	# 	return false
+	# return currentItem.onUseStove()
+	return false
 
-	currentItem = get_node("/root/Node3D/Player").request_drop_held_item(self)
-	currentItem.obj_attached_to = self
-	currentItem.position = $ItemAnchor.global_position
-	return currentItem.onUseStove()
+func attach_item(_item):
+	# if item.has_method("onUseStove"):
+	# 	item.onUseStove()
+	update_state("full")
 
-func takeItem():
-	currentItem.obj_attached_to = null
-	currentItem = null
+func take_item():
+	update_state("empty")
 
-func is_item_allowed(itemType):
-	return allow_list.size() > 0 and itemType in allow_list
+func update_state(new_state):
+	if new_state == state:
+		return
+	state = new_state
+	#state_changed.emit(new_state)
 	
 func getName():
 	return item_type
