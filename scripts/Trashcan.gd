682b5ff7b3a19a36db348b05946bc838bf720f8f
Danielle Purdes <daniellepurdes@gmail.com>
2024-11-24 18:41:53 -0800

    Improve is_item_allowed()

--- scripts/Trashcan.gd
+++ scripts/Trashcan.gd
@@ -6,15 +6,15 @@ class_name Trashcan
 @export var allow_list:Array[String] = ["tea_brick"]
 
 func useItem(item):
-	if item == null:
-		return false
-	if "item_type" not in item:
-		return false
-	if !is_item_allowed(item.item_type):
+	if !is_item_allowed(item):
 		return false
 	
 	get_node("/root/Node3D/Player").request_drop_held_item(self)
 	return true
 	
-func is_item_allowed(item_type):
-	return allow_list.size() > 0 and item_type in allow_list
+func is_item_allowed(item):
+	if item == null:
+		return false
+	if "item_type" not in item:
+		return false
+	return allow_list.is_empty() or item.item_type in allow_list
